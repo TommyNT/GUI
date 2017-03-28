bafa8ca75fd9b3baa2e81696866052cdac6c7df4
Duong Nguyen Tuan <TommyNT@MacBook-Pro-Duong.local>
2017-03-28 13:47:15 +0200

    GUI:

---
+++ www/js/pgb.js
@@ -0,0 +1,15 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(2);
+	deviceInfo();
+}
+
+function deviceInfo() {
+
+	content =  'The graphical user interface (GUI), is a type of user interface that allows users to interact with electronic devices through graphical icons and visual indicators such as secondary notation, instead of text-based user interfaces, typed command labels or text navigation. GUIs were introduced in reaction to the perceived steep learning curve of command-line interfaces (CLIs), which require commands to be typed on a computer keyboard.';
+
+	document.getElementById("deviceDetails").innerHTML = content;	
+}
